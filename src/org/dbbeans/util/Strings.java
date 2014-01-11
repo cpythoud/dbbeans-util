bef598c5b50b414bade687d18fe548a600b86cff
Christophe Pythoud <git@pythoud.net>
2014-01-11 16:40:20 -0700

    Addes functions to (un)capitalize and (un)camelize strings to Strings.

--- src/org/dbbeans/util/Strings.java
+++ src/org/dbbeans/util/Strings.java
@@ -1,5 +1,6 @@
 package org.dbbeans.util;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,6 +8,87 @@ import java.util.List;
  * This class contains static function to help with Strings manipulations.
  */
 public class Strings {
+
+    /**
+     * Capitalize a String.
+     * If the first character of the String is lowercase, an identical String is returned with this first character converted to uppercase.
+     * An identical String is returned otherwise.
+     * @param string to be capitalized.
+     * @return capitalized String.
+     */
+    public static String capitalize(String string) {
+        return string.substring(0, 1).toUpperCase() + string.substring(1, string.length());
+    }
+
+    /**
+     * Uncapitalize a String.
+     * If the first character of the String is uppercase, an identical String is returned with this first character converted to lowercase.
+     * An identical String is returned otherwise.
+     * @param string to be uncapitalized.
+     * @return uncapitalized String.
+     */
+    public static String uncapitalize(String string) {
+        return string.substring(0, 1).toLowerCase() + string.substring(1, string.length());
+    }
+
+    /**
+     * Camelize a String by suppressing underscores and capitalizing the following letter.
+     * Given a String of the form 'some_kind_of_string', this function returns a String of the form 'someKindOfString'.
+     * This function is mostly used in programs that generate or manipulate source code.
+     * @param string to be camelized.
+     * @return camelizedString.
+     * @throws java.lang.IllegalArgumentException if the String contains anything but alphanumeric ASCII characters, or
+     * the String starts or ends with an underscore characters, or the String contains two consecutive underscore
+     * characters.
+     */
+    public static String camelize(final String string) {
+        if (!string.matches("^[a-zA-Z0-9_]+$"))
+            throw new IllegalArgumentException("Illegal identifier character");
+        if (string.startsWith("_"))
+            throw new IllegalArgumentException("String cannot start with underscore character");
+        if (string.contains("__"))
+            throw new IllegalArgumentException("String cannot contain two consecutive underscore characters");
+        if (string.endsWith("_"))
+            throw new IllegalArgumentException("String cannot end with underscore character");
+        String[] parts = string.split("_");
+        StringBuilder buf = new StringBuilder();
+        for (String part: parts)
+            buf.append(capitalize(part.toLowerCase()));
+        return buf.toString();
+    }
+
+    /**
+     * Uncamelize a String by transforming any uppercase letter to its lowercase equivalent and preceding it by an
+     * underscore.
+     * Given a String of the form 'someKindOfString', this function returns a String of the form 'some_kind_of_string'.
+     * This function is mostly used in programs that generate or manipulate source code.
+     * @param string to be uncamelized.
+     * @return uncamelized String.
+     * @throws java.lang.IllegalArgumentException if the String contains anything but alphanumeric ASCII characters.
+     */
+    public static String uncamelize(final String string) {
+        if (!string.matches("^[a-zA-Z0-9]+$"))
+            throw new IllegalArgumentException("Illegal identifier character");
+        List<String> parts = new ArrayList<String>();
+        StringBuilder buf = new StringBuilder();
+        for (int i = 0; i < string.length(); i++) {
+            char c = string.charAt(i);
+            if (i > 0 && c >= 'A' && c <= 'Z') {
+                parts.add(buf.toString());
+                buf = new StringBuilder();
+            }
+            buf.append(c);
+        }
+        if (buf.length() > 0)
+            parts.add(buf.toString());
+        buf = new StringBuilder();
+        for (int i = 0; i < parts.size() - 1; i++) {
+            buf.append(parts.get(i));
+            buf.append('_');
+        }
+        buf.append(parts.get(parts.size() - 1));
+        return buf.toString().toLowerCase();
+    }
 
     /**
      * Concatenate the Strings passed as parameters.
