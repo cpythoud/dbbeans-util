a3436827fdbdf201454d49622f02f38bc2c37064
Christophe Pythoud <git@pythoud.net>
2014-01-11 13:56:13 -0700

    Added String concatenation functions to Strings.

--- src/org/dbbeans/util/Strings.java
+++ src/org/dbbeans/util/Strings.java
@@ -1,8 +1,60 @@
 package org.dbbeans.util;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
- * ...
+ * This class contains static function to help with Strings manipulations.
  */
 public class Strings {
+
+    /**
+     * Concatenate the Strings passed as parameters.
+     * @param strings to be concatenated.
+     * @return the result of the concatenation.
+     */
+    public static String concat(final String... strings) {
+        return concat(Arrays.asList(strings));
+    }
+
+    /**
+     * Takes a list of Strings and concatenate them together.
+     * @param strings a list of Strings to be concatenated together.
+     * @return the result of the concatenation.
+     */
+    public static String concat(final List<String> strings) {
+        final StringBuilder buf = new StringBuilder();
+        for (String s: strings)
+            buf.append(s);
+        return buf.toString();
+    }
+
+    /**
+     * Concatenate Strings together, inserting a specified separator between them.
+     * @param separator to be inserted between Strings.
+     * @param strings to be concatenated.
+     * @return the result of the concatenation.
+     */
+    public static String concatWithSeparator(final String separator, final String... strings) {
+        return concatWithSeparator(separator, Arrays.asList(strings));
+    }
+
+    /**
+     * Takes a list of Strings and concatenate them together, inserting a specified separator between them.
+     * @param separator to be inserted between Strings.
+     * @param strings a list of Strings to be concatenated together.
+     * @return the result of the concatenation.
+     */
+    public static String concatWithSeparator(final String separator, final List<String> strings) {
+        final StringBuilder buf = new StringBuilder();
+
+        for (String s: strings) {
+            buf.append(s);
+            buf.append(separator);
+        }
+        buf.delete(buf.length() - separator.length(), buf.length());
+
+        return buf.toString();
+    }
 
 }
