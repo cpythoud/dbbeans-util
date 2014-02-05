de319d2b8ff1fdfb91b2d3112d82c820579e35b9
Christophe Pythoud <git@pythoud.net>
2014-02-05 15:54:22 -0700

    Added javadoc and made some function parameter names explicit.

--- src/org/dbbeans/util/SimpleInputDateFormat.java
+++ src/org/dbbeans/util/SimpleInputDateFormat.java
@@ -5,14 +5,36 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * ...
+ * This class is used to validate dates input. It allows for different orderings of dates elements (day,
+ * month, year) and varying separators between these elements.
  */
 public class SimpleInputDateFormat {
 
+    /**
+     * Here are defined constants to specify the order of elements (day, month, year) in a date format.
+     */
     public static enum ElementOrder {
-        YYMD, DMYY, MDYY
+        /**
+         * Represents the following date format: 4-digits-year separator 2-digits-month separator 2-digits-day-of-month.
+         */
+        YYMD,
+        /**
+         * Represents the following date format: 2-digits-day-of-month separator 2-digits-month separator 4-digits-year.
+         */
+        DMYY,
+        /**
+         * Represents the following date format: 2-digits-month separator 2-digits-day-of-month separator 4-digits-year.
+         */
+        MDYY
     }
 
+    /**
+     * Create a SimpleInputDateFormat, specifying the order of the date elements (day, month, year) and the
+     * separator used between these elements.
+     * @param order of the date elements.
+     * @param separator between the elements.
+     * @see SimpleInputDateFormat#SimpleInputDateFormat(SimpleInputDateFormat.ElementOrder, java.util.List)
+     */
     public SimpleInputDateFormat(final ElementOrder order, final String separator) {
         this.order = order;
         List<String> seps = new ArrayList<String>();
@@ -20,6 +42,13 @@ public class SimpleInputDateFormat {
         this.separators = Collections.unmodifiableList(seps);
     }
 
+    /**
+     * Create a SimpleInputDateFormat, specifying the order of the date elements (day, month, year) and a list of
+     * possible separators used between these elements.
+     * @param order of the date elements.
+     * @param separators that can be used between the elements.
+     * @see SimpleInputDateFormat#SimpleInputDateFormat(SimpleInputDateFormat.ElementOrder, String)
+     */
     public SimpleInputDateFormat(final ElementOrder order, final List<String> separators) {
         this.order = order;
         List<String> seps = new ArrayList<String>();
@@ -27,27 +56,40 @@ public class SimpleInputDateFormat {
         this.separators = Collections.unmodifiableList(seps);
     }
 
+    /**
+     * Get the expected order of date elements (day, month, year) for this SimpleDateFormat.
+     * @return the expected order of date elements.
+     */
     public ElementOrder getOrder() {
         return order;
     }
 
+    /**
+     * Get a list of separators that can be used between date elements (day, month, year) for this SimpleDateFormat.
+     * @return a list of separators that can be used between date elements.
+     */
     public List<String> getSeparators() {
         return separators;
     }
 
-    public boolean validate(final String dateString) {
+    /**
+     * Validate a date according to this SimpleDateFormat.
+     * @param date to be validated.
+     * @return true if the date is correct according to the format, false otherwise.
+     */
+    public boolean validate(final String date) {
         for (String separator: separators) {
             switch (order) {
                 case YYMD:
-                    if (Dates.isYYMDDateOK(dateString, separator))
+                    if (Dates.isYYMDDateOK(date, separator))
                         return true;
                     break;
                 case DMYY:
-                    if (Dates.isDMYYDateOK(dateString, separator))
+                    if (Dates.isDMYYDateOK(date, separator))
                         return true;
                     break;
                 case MDYY:
-                    if (Dates.isMDYYDateOK(dateString, separator))
+                    if (Dates.isMDYYDateOK(date, separator))
                         return true;
                     break;
                 default:
