6d8ecb7f6b332cee0f80214c2896989a7d6712d9
Christophe Pythoud <git@pythoud.net>
2014-02-05 17:21:31 -0700

    Added javadoc.

--- src/org/dbbeans/util/IPv4Address.java
+++ src/org/dbbeans/util/IPv4Address.java
@@ -3,10 +3,17 @@ package org.dbbeans.util;
 import java.util.regex.Pattern;
 
 /**
- * ...
+ * This class is used to represent and validate IPv4 Addresses.
+ * This class is used by {@link EmailValidator} to determine if the IP part of an e-mail address is correct.
  */
 public class IPv4Address {
 
+    /**
+     * The constructor expects an array of four ints representing an IPv4 address.
+     * @param bytes representing an IPv4 address.
+     * @throws java.lang.IllegalArgumentException if the array length is not 4 or if any of the ints is not an
+     * acceptable value (i.e., comprised between 0 and 255).
+     */
     public IPv4Address(final int... bytes) {
         if (bytes.length != 4)
             throw new IllegalArgumentException("Illegal number of bytes in IP v4 Address: " + bytes.length + " (must be 4).");
@@ -18,10 +25,19 @@ public class IPv4Address {
         setBytes(bytes);
     }
 
+    /**
+     * Copy constructor.
+     * @param address to be copied.
+     */
     public IPv4Address(final IPv4Address address) {
         setBytes(address.bytes);
     }
 
+    /**
+     * Creates an IPv4Address from a string representation of an IPv4 address.
+     * @param address string representation of an IPv4 address.
+     * @see IPv4Address#valueOf(String)
+     */
     public IPv4Address(final String address) {
         this(valueOf(address));
     }
@@ -30,6 +46,11 @@ public class IPv4Address {
         System.arraycopy(bytes, 0, this.bytes, 0, 4);
     }
 
+    /**
+     * Checks if a String is a valid representation of an IPv4 address.
+     * @param address to be checked.
+     * @return true if <code>address</code> is a valid representation of an IPv4 address, false otherwise.
+     */
     public static boolean isValid(final String address) {
         final String[] parts = address.split(".");
         if (parts.length != 4)
@@ -42,6 +63,12 @@ public class IPv4Address {
         return true;
     }
 
+    /**
+     * Creates an IPv4Address from a string representation of an IPv4 address.
+     * @param address string representation of an IPv4 address.
+     * @return IPv4Address created.
+     * @see IPv4Address#IPv4Address(String)
+     */
     public static IPv4Address valueOf(final String address) {
         if (!isValid(address))
             throw new IllegalArgumentException("Invalid IP Address Format: " + address);
