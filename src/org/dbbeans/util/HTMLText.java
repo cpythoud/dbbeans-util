229478016e8236fef26a7011290f1e2787dda273
cpythoud <git@pythoud.net>
2017-07-07 18:18:30 -0700

    Clean up commented code

--- src/org/dbbeans/util/HTMLText.java
+++ src/org/dbbeans/util/HTMLText.java
@@ -161,49 +161,4 @@ public class HTMLText {
     private final static String[] ESCAPE_PATTERN = CharacterLists.getEscapePatternArray();
 
     private final static String[] ACCENTED_CHARACTERS = CharacterLists.getAccentedCharacterArray();
-
-    /*private final static String[] NUMBER_PATTERN = {
-            "&#225;", "&#224;", "&#226;", "&#228;",
-            "&#233;", "&#232;", "&#234;", "&#235;",
-            "&#237;", "&#236;", "&#238;", "&#239;",
-            "&#243;", "&#242;", "&#244;", "&#246;",
-            "&#250;", "&#249;", "&#251;", "&#252;",
-            "&#193;", "&#192;", "&#194;", "&#196;",
-            "&#201;", "&#200;", "&#202;", "&#203;",
-            "&#205;", "&#204;", "&#206;", "&#207;",
-            "&#211;", "&#210;", "&#212;", "&#214;",
-            "&#218;", "&#217;", "&#219;", "&#220;",
-            "&#231;", "&#199;", "&#253;", "&#221;",
-            "&#227;", "&#241;", "&#245;",
-            "&#195;", "&#209;", "&#213;" };
-
-    private final static String[] ESCAPE_PATTERN = {
-            "&aacute;", "&agrave;", "&acirc;", "&auml;",
-            "&eacute;", "&egrave;", "&ecirc;", "&euml;",
-            "&iacute;", "&igrave;", "&icirc;", "&iuml;",
-            "&oacute;", "&ograve;", "&ocirc;", "&ouml;",
-            "&uacute;", "&ugrave;", "&ucirc;", "&uuml;",
-            "&Aacute;", "&Agrave;", "&Acirc;", "&Auml;",
-            "&Eacute;", "&Egrave;", "&Ecirc;", "&Euml;",
-            "&Iacute;", "&Igrave;", "&Icirc;", "&Iuml;",
-            "&Oacute;", "&Ograve;", "&Ocirc;", "&Ouml;",
-            "&Uacute;", "&Ugrave;", "&Ucirc;", "&Uuml;",
-            "&ccedil;", "&Ccedil;", "&yacute;", "&Yacute;",
-            "&atilde;", "&ntilde;", "&otilde;",
-            "&Atilde;", "&Ntilde;", "&Otilde;" };
-
-    private final static String[] ACCENTED_CHARACTERS = {
-            "á", "à", "â", "ä",
-            "é", "è", "ê", "ë",
-            "í", "ì", "î", "ï",
-            "ó", "ò", "ô", "ö",
-            "ú", "ù", "û", "ü",
-            "Á", "À", "Â", "Ä",
-            "É", "È", "Ê", "Ë",
-            "Í", "Ì", "Î", "Ï",
-            "Ó", "Ò", "Ô", "Ö",
-            "Ú", "Ù", "Û", "Ü",
-            "ç", "Ç", "ý", "Ý",
-            "ã", "ñ", "õ",
-            "Ã", "Ñ", "Õ" };*/
 }
