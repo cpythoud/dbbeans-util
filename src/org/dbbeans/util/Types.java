d6dad91cfb9051d806d323d59f3a45d72405dc2b
cpythoud <git@pythoud.net>
2018-03-21 16:34:38 -0700

    Simplify getSubtypeList code

--- src/org/dbbeans/util/Types.java
+++ src/org/dbbeans/util/Types.java
@@ -6,10 +6,6 @@ import java.util.List;
 public class Types {
 
     public static <T> List<T> getSubtypeList(final List<? extends T> supertypes) {
-        final List<T> subtypes = new ArrayList<T>();
-
-        subtypes.addAll(supertypes);
-
-        return subtypes;
+        return new ArrayList<T>(supertypes);
     }
 }
