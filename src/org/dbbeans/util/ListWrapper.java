247babb5387cbcaa8a05f165faa55cc56b5d8947
cpythoud <git@pythoud.net>
2018-06-01 20:32:23 -0700

    Create generic List wrapper.
    For when you REALLY need to pass list of different types to
    a constructor.
    
    Pattern: MyObjectListWrapper extends ListWrapper<MyObject>

---
+++ src/org/dbbeans/util/ListWrapper.java
@@ -0,0 +1,18 @@
+package org.dbbeans.util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public abstract class ListWrapper<T> {
+
+    private final List<T> list = new ArrayList<T>();
+
+    protected void initializeList(final List<T> list) {
+        this.list.addAll(list);
+    }
+
+    public List<T> getList() {
+        return Collections.unmodifiableList(list);
+    }
+}
